124b17087e91329ae6e680a913782b7764eb0073
ridho007 <mzulfajrin98@guru.sd.belajar.id>
2024-09-18 21:09:21 +0800

    File Upload

---
+++ story_content/user.js
@@ -0,0 +1,153 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "619KkssSQgr":
+        Script1();
+        break;
+      case "5YGKS4DGQPg":
+        Script2();
+        break;
+      case "65TSRHt4lwv":
+        Script3();
+        break;
+      case "5kvvyRyOsMe":
+        Script4();
+        break;
+      case "6Ca9LTJp0JA":
+        Script5();
+        break;
+      case "6jsr4uAkqXB":
+        Script6();
+        break;
+      case "6ASIjxzUyev":
+        Script7();
+        break;
+      case "6Wwl0aoawF4":
+        Script8();
+        break;
+      case "6mTz9XigkGc":
+        Script9();
+        break;
+      case "5pUQxzzBoM0":
+        Script10();
+        break;
+      case "6El2rOnPOLj":
+        Script11();
+        break;
+      case "6WYqoFK134j":
+        Script12();
+        break;
+      case "6AP2pDBFnmp":
+        Script13();
+        break;
+      case "60r87PH00vg":
+        Script14();
+        break;
+      case "6MutTMHQ1XI":
+        Script15();
+        break;
+      case "5f6jOCkhWCt":
+        Script16();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="musik.mp3";
+audio.load();
+audio.play();
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.2;
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.0;
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.0;
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.1;
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.2;
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.3;
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.4;
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.5;
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.6;
+}
+
+function Script11()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.7;
+}
+
+function Script12()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.8;
+}
+
+function Script13()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.9;
+}
+
+function Script14()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 1.0;
+}
+
+function Script15()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.0;
+}
+
+function Script16()
+{
+  var audio = document.getElementById('bgSong');
+audio.volume = 0.2;
+}
+
